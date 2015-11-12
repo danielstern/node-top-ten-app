c3d1ef0b5939ac1fe5989870f25e78758f8be311
Code Whisperer <daniel@danielstern.ca>
2015-11-12 13:21:49 -0500

    implement items route

--- server/server.js
+++ server/server.js
@@ -3,14 +3,15 @@ let port = 7777;
 var db = require('./db.js');
 var express = require('express');
 var app = new express();
+let TopTenListItem = require('./models/TopTenListItem.js');
 
 app.use(express.static('app'));
 
 app.route(`/api/items`)
 .get((req,res)=>{
-//	Mongoose.Model(	.find(function(error,doc){
-//		res.send(doc);
-//	})
+	TopTenListItem.find(function(error,doc){
+		res.send(doc);
+	});
 })
 
 app.listen(port,()=>{console.log(`App listening on port ${port}.`);});
