0bc14786d29af0e04de8076d52d4eb65f7671c13
Code Whisperer <daniel@danielstern.ca>
2015-11-13 17:51:56 -0500

    auth near completion

--- app/main.js
+++ app/main.js
@@ -1,1 +1,13 @@
-console.info("App init!");
+"use strict";
+let react = require('react');
+let $ = require('jquery');
+
+$.ajax({
+	url:'/login',
+	type:'POST',
+	data:{username:'guest',password:'test'},
+	success:a=>console.log(a),
+	error:z=>console.warn(z)
+})
+
+console.info("App init");
--- server/auth.js
+++ server/auth.js
@@ -2,23 +2,38 @@
 module.exports = function(app){
 	let passport = require('passport');
 	let LocalStrategy = require('passport-local');
-	passport.use(new LocalStrategy({
-		usernameField: 'email',
-		passwordField: 'password'
-	  },
-	  function(username, password, done) {
+	let local = new LocalStrategy(function(username, password, done) {
+		console.log("Attempting login:", username);
 		User.findOne({ username: username }, function (err, user) {
-		  if (err) { return done(err); }
-		  if (!user) { return done(null, false); }
-		  if (!user.verifyPassword(password)) { return done(null, false); }
-		  return done(null, user);
+			if (err) { return done(err); }
+			if (!user) { return done(null, false); }
+			if (!user.verifyPassword(password)) { return done(null, false); }
+			return done(null, user);
 		});
-	  }
-	));
+	});
+	
+	passport.use(local);
 
-	app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }),
-	function(req, res) {
-		res.redirect('/');
+//	app.post('/login', passport.authenticate('local'), function(req, res) {	
+//		console.log("Login");
+//		res.status(301).send();
+//	});
+	
+	app.post('/login', function(req, res, next) {
+	  console.log("Login");
+	  passport.authenticate('local', function(err, user, info) {
+		
+		if (err) { return next(err); }
+		if (!user) { 
+			console.log("error no user");
+			res.status(404).send();
+			//return res.redirect('/login'); 
+		}
+	//	req.logIn(user, function(err) {
+	//	  if (err) { return next(err); }
+		  //return res.redirect('/users/' + user.username);
+	//	});
+	  })(req, res, next);
 	});
 
 	passport.serializeUser(function(user, cb) {
