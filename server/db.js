2c0e2a817c3293abf756dc3d63a207d9ddbf765c
Code Whisperer <daniel@danielstern.ca>
2015-11-13 12:36:01 -0500

    install passport and passport local deps

--- server/db.js
+++ server/db.js
@@ -5,11 +5,14 @@ let User = require('./models/User.js');
 let TopTenList = require('./models/TopTenList.js');
 let TopTenListItem = require('./models/TopTenListItem.js');
 
-mongoose.connect(`mongodb://localhost/grocery`,()=>{	
+mongoose.connect(`mongodb://localhost/topten`,()=>{	
 	mongoose.connection.db.dropDatabase();
-
+	
+	let list = new TopTenList({name:`Favorite Songs`}).save();
+	let user = new User();
+	
 	let initialItems = [1,2,3,4,5,6,7,8,9,10]
-		.map((n)=>{return {name:`Item ${n}`, rank:n}});
+		.map((n)=>{return {name:`Item ${n}`, rank:n, list:list._id}});
 
 	initialItems.forEach(function(item){
 		new TopTenListItem(item).save();
--- server/server.js
+++ server/server.js
@@ -4,14 +4,21 @@ var db = require('./db.js');
 var express = require('express');
 var app = new express();
 let TopTenListItem = require('./models/TopTenListItem.js');
+let TopTenList = require('./models/TopTenList.js');
 
 app.use(express.static('app'));
 
 app.route(`/api/items`)
 .get((req,res)=>{
-	TopTenListItem.find(function(error,doc){
+	TopTenListItem.find(function(error, doc){
+		res.send(doc);
+	});
+});
+app.route(`/api/lists`)
+.get((req,res)=>{
+	TopTenList.find(function(error, doc){
 		res.send(doc);
 	});
 })
 
-app.listen(port,()=>{console.log(`App listening on port ${port}.`);});
+app.listen(port,()=>{console.log(`App listening on port ${port}.`)});
