527e1c3d05d39a0d1de53377115d77dcbd8339d0
Code Whisperer <daniel@danielstern.ca>
2016-01-16 06:59:04 -0500

    fix mongo

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 "use strict";
 let mongoose = require(`mongoose`);
 
-mongoose.connect(process.env.NODE_ENV == 'production' ? `mongodb://heroku_drx2dp31:35a0gtdvl0gmqhvkejrrjion9s@ds047305.mongolab.com:47305/heroku_drx2dp31` : `mongodb://localhost/topten`,()=>{	
+mongoose.connect(process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL || `mongodb://localhost/topten`,()=>{	
 	mongoose.connection.db.dropDatabase(function(){
 		require('./seed.js');	
 	});
